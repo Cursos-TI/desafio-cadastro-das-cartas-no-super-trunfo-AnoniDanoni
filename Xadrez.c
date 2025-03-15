9a28744b07b30b80f97247ba5b72a6268ea732d5
AnoniDanoni <andersonanoni2810@gmail.com>
2025-03-15 17:06:01 +0000

    Desafio Xadrez: nivel mestre, feito

--- Xadrez.c
+++ Xadrez.c
@@ -1,29 +1,41 @@
 #include <stdio.h>
+
+void movimento_rainha(){
+
+    printf("rainha:\n");
+    printf("esquerda\nesquerda\nesquerda\nesquerda\nesquerda\nesquerda\nesquerda\nesquerda\n");
+            printf("-----------------------------------\n");
+}
+
+
+
+void movimento_bispo() {
+    printf("bispo:\n");
+    for (int linha = 1; linha <= 5; linha++) {
+        for (int coluna = 1; coluna <= 2; coluna++) {
+            printf(coluna == 1 ? "direita  " : "cima  ");
+        }
+        printf("\n"); 
+    }
+    printf("-----------------------------------\n");
+}
+
+
+void movimento_torre(){
+    
+    printf("torre:\n");
+            printf("esquerda\nesquerda\nesquerda\nesquerda\nesquerda\n");
+            printf("-----------------------------------\n");
+}
+
+
+
 int main()
 {
+    movimento_bispo();
+    movimento_rainha();
+    movimento_torre();
 
-    printf("torre:\n");
-        for (int torre = 1; torre <= 5; 
-        torre++) {
-            printf("esquerda\n");
-        }
-            printf("-----------------------------------\n");
-            
-    printf("bispo:\n");
-        for (int bispo = 1; bispo <= 5; 
-        bispo++) {
-            printf("cima, direita\n");
-        }
-                printf("-----------------------------------\n");
-    
-    
-        printf("rainha:\n");
-        for (int rainha = 1; rainha <= 8; 
-        rainha++) {
-            printf("esquerda\n");
-        }
-                printf("-----------------------------------\n");
-        
         
             printf("Cavalo:\n");
             for (int cavalov = 1; cavalov <= 2; cavalov++) {

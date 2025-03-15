b6729ca54f94f4c10bb09b2276f1fca1abba0805
AnoniDanoni <andersonanoni2810@gmail.com>
2025-03-15 01:09:30 +0000

    Desafio Xadrez: Nivel novato, feito

---
+++ Xadrez.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+
+    printf("torre:\n");
+        for (int torre = 1; torre <= 5; 
+        torre++) {
+            printf("esquerda\n");
+        }
+            printf("-----------------------------------\n");
+            
+    printf("bispo:\n");
+        for (int bispo = 1; bispo <= 5; 
+        bispo++) {
+            printf("cima, direita\n");
+        }
+                printf("-----------------------------------\n");
+    
+    
+        printf("rainha:\n");
+        for (int rainha = 1; rainha <= 8; 
+        rainha++) {
+            printf("esquerda\n");
+        }
+                printf("-----------------------------------\n");
+        return 0
+}
